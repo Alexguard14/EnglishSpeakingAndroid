53e92c098f4dc9d4bbf042c54ed1d67d40fd1e11
MekaroStudio <68746149+mekarostudio@users.noreply.github.com>
2020-09-24 13:15:57 -0700

    Update 24/09/20

--- app/src/main/java/com/mekarostudio/englishspeakingandroid/MainActivity.java
+++ app/src/main/java/com/mekarostudio/englishspeakingandroid/MainActivity.java
@@ -1,8 +1,12 @@
 package com.mekarostudio.englishspeakingandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        toolbar.setTitle("Home");
+        toolbar.setSubtitle("Welcome Erick");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+
+
+        return true;
     }
 }
